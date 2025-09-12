b9b5dcae4e285cd1bab1b7564c927015f8086068
Arshadali04 <arshadali.athani@gmail.com>
2025-09-12 12:19:45 +0530

    On branch main # Your branch is up to date with 'origin/main'.  # # Changes to be committed: #	modified: anonymized_bank_transactions.csv #	renamed: bank_transactions.csv -> bank_details.csv #	new file:   date.txt # modified:   main.py
    AAA

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 import pandas as pd
-
-data=pd.read_csv("bank_transactions.csv")
+data=pd.read_csv("bank_details.csv")
 def generalize_age(age):
-    if age < 35:
-        return "35-";
-    elif age < 40:
+    if age < 20:
+        return  "15-19";
+    elif age < 25 and age >= 20:  
+        return "20-24";
+    elif age < 30 and age >= 25:
+        return "25-29";
+    elif age < 35  and age >= 30:
+        return "30-34";
+    elif age < 40 and age >= 35:
         return "35-39";
-    elif age < 45:
+    elif age < 45 and age >= 40:
         return "40-44";
-    elif age < 50:
+    elif age < 50 and age >= 45:
         return "45-49";
-    elif age < 55:
+    elif age < 55 and age >= 50:
         return "50-54";
-    else:
-        return "55+";
+    elif age >= 55:
+        return "55+"; 
 
 
 if __name__ == "__main__":
-    data["CustomerDOB"] = pd.to_datetime(data["CustomerDOB"], format="%Y-%m-%d", errors='coerce')
-    today = pd.to_datetime('2025-09-11')
-    data["age"] = ((today - data["CustomerDOB"]).dt.days / 365.25).astype('Int64', errors='ignore')
     data["age"] = data["age"].apply(generalize_age)
-    data = data.drop(columns=["CustomerDOB"])
     data.to_csv("anonymized_bank_transactions.csv", index=False)
     print("Anonymization complete. Output saved to anonymized_bank_transactions.csv")
